59c78882bfc8a6e6fd608197d18113a5c6692ae0
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-19 16:33:43 +0330

    create location app

--- instagram/settings.py
+++ instagram/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "activity.apps.ActivityConfig",
     "content.apps.ContentConfig",
+    "location.apps.LocationConfig",
 ]
 
 MIDDLEWARE = [
