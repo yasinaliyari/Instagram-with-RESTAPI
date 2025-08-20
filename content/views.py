c40e47f6a604d6b223a7b70e6434d4877b317637
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-20 12:44:18 +0330

    update TagListAPI and create TagDetailAPI with url

--- content/views.py
+++ content/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from content.models import Tag
+
+
+class TagDetailAPI(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        tag = get_object_or_404(Tag, **{"pk": pk})
+
+        return Response(
+            {
+                "id": tag.id,
+                "title": tag.title,
+                "posts": tag.posts.count(),
+            },
+            status=status.HTTP_200_OK,
+        )
 
 
 class TagListAPI(APIView):
     def get(self, request, *args, **kwargs):
-        tags = Tag.objects.all().values("title")
-        return Response(tags)
+        tags = Tag.objects.all()
+
+        data = list()
+
+        for tag in tags:
+            data.append(
+                {
+                    "id": tag.id,
+                    "title": tag.title,
+                    "posts": tag.posts.count(),
+                }
+            )
+        return Response(data, status=status.HTTP_200_OK)
