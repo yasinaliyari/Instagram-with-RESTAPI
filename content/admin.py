25708c75d32b2b025fb33d4cd7982a8fed6c4241
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-20 20:32:27 +0330

    register content models

--- content/admin.py
+++ content/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.admin import register
-from content.models import Post, Tag
+from content.models import Post, PostMedia, Tag, PostTag, TaggedUser
 
 
 @register(Post)
@@ -11,9 +11,24 @@ class PostAdmin(admin.ModelAdmin):
     )
 
 
+@register(PostMedia)
+class PostMediaAdmin(admin.ModelAdmin):
+    list_display = ("media_type",)
+
+
 @register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = (
         "title",
         "created_time",
     )
+
+
+@register(PostTag)
+class PostTagAdmin(admin.ModelAdmin):
+    list_display = ("tag",)
+
+
+@register(TaggedUser)
+class TaggedUserAdmin(admin.ModelAdmin):
+    list_display = ("user",)
