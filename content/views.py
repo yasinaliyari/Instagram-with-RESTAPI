5466aa382c8d2f923f50696ceec4545e412689b0
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-20 19:25:08 +0330

    update get in TagDetailAPI, TagListAPI and add post to TagListAPI

--- content/views.py
+++ content/views.py
@@ -1,37 +1,27 @@
-from django.http import Http404
 from rest_framework.generics import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from content.models import Tag
+from content.serializers import TagListSerializer, TagDetailSerializer
 
 
 class TagDetailAPI(APIView):
     def get(self, request, pk, *args, **kwargs):
         tag = get_object_or_404(Tag, **{"pk": pk})
-
-        return Response(
-            {
-                "id": tag.id,
-                "title": tag.title,
-                "posts": tag.posts.count(),
-            },
-            status=status.HTTP_200_OK,
-        )
+        serializer = TagDetailSerializer(tag)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
 
 class TagListAPI(APIView):
     def get(self, request, *args, **kwargs):
         tags = Tag.objects.all()
+        serializer = TagListSerializer(tags, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-        data = list()
-
-        for tag in tags:
-            data.append(
-                {
-                    "id": tag.id,
-                    "title": tag.title,
-                    "posts": tag.posts.count(),
-                }
-            )
-        return Response(data, status=status.HTTP_200_OK)
+    def post(self, request, *args, **kwargs):
+        serializer = TagListSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
