095a2e793a4d876dae7a4f6313d1529c5e4bb263
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-20 20:41:43 +0330

    some changes

--- content/serializers.py
+++ content/serializers.py
@@ -1,18 +1,26 @@
 from rest_framework import serializers
-from content.models import Tag
+from content.models import Tag, Post, PostMedia
+from location.serializers import LocationSerializer
 
 
-class TagListSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField()
+class TagListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = (
+            "id",
+            "title",
+        )
 
-    def create(self, validated_data):
-        instance = Tag.objects.create(**validated_data)
-        return instance
 
-
-class TagDetailSerializer(TagListSerializer):
+class TagDetailSerializer(serializers.ModelSerializer):
     posts = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Tag
+        fields = (
+            "id",
+            "title",
+        )
 
     def get_posts(self, obj):
         return obj.posts.count()
