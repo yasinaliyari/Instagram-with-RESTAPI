f7cf59a2b0cbeeb48c2b30cef24a15c86a9a1ea0
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-24 00:07:51 +0330

    create CommentCreateAPIView class

---
+++ activity/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from activity.views import CommentCreateAPIView
+
+urlpatterns = [
+    path("comment/create/", CommentCreateAPIView.as_view(), name="comment-create")
+]
--- activity/views.py
+++ activity/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated
+from activity.models import Comment
+from activity.serializers import CommentCreateSerializer
 
-# Create your views here.
+
+class CommentCreateAPIView(CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentCreateSerializer
+    permission_classes = (IsAuthenticated,)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
