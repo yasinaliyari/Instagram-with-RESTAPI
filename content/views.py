5420c864b4cd54d3e6ae56fa7973c1042e41f124
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-23 23:34:04 +0330

    use ListCreateAPIView

--- content/views.py
+++ content/views.py
@@ -1,5 +1,5 @@
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.generics import get_object_or_404, ListAPIView
+from rest_framework.generics import get_object_or_404, ListAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -21,7 +21,7 @@ class TagDetailAPI(APIView):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 
-class TagListAPI(ListAPIView):
+class TagListAPI(ListCreateAPIView):
     queryset = Tag.objects.all()
     serializer_class = TagListSerializer
     permission_classes = (IsAuthenticated,)
@@ -31,12 +31,12 @@ class TagListAPI(ListAPIView):
     #     serializer = TagListSerializer(tags, many=True)
     #     return Response(serializer.data, status=status.HTTP_200_OK)
 
-    def post(self, request, *args, **kwargs):
-        serializer = TagListSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    # def post(self, request, *args, **kwargs):
+    #     serializer = TagListSerializer(data=request.data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(status=status.HTTP_201_CREATED)
+    #     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class PostDetailAPI(APIView):
