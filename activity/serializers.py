48c2c83443f5747c3b44e856e4b42e6c33a3c679
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-24 02:24:30 +0330

    use validate for caption

--- activity/serializers.py
+++ activity/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
+
 from activity.models import Comment
 from content.serializers import PostDetailSerializer
 
@@ -7,6 +9,11 @@ class CommentCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ("caption", "post", "reply_to")
+
+    def validate(self, attrs):
+        if len(attrs["caption"]) > 30:
+            raise ValidationError("Caption cannot be more than 30 characters")
+        return attrs
 
 
 class CommentListSerializer(serializers.ModelSerializer):
