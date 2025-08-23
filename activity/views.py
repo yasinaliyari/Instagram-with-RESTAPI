86d69312d8c84ae6b214edacce02247c979ce5ec
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-24 00:14:05 +0330

    change to ListCreateAPIView

--- activity/views.py
+++ activity/views.py
@@ -1,10 +1,10 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from activity.models import Comment
 from activity.serializers import CommentCreateSerializer
 
 
-class CommentCreateAPIView(CreateAPIView):
+class CommentListCreateAPIView(ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentCreateSerializer
     permission_classes = (IsAuthenticated,)
