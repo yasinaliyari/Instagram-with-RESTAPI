bba69ce77a1a6a971dce4684b6f0349aac9452ec
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-24 03:02:54 +0330

    show comments with post

--- content/serializers.py
+++ content/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from activity.serializers import CommentListSerializer
 from content.models import Tag, Post, PostMedia
 from location.serializers import LocationSerializer
 
@@ -40,12 +41,21 @@ class PostDetailSerializer(serializers.ModelSerializer):
     user = serializers.CharField(source="user.username")
     location = LocationSerializer()
     media = PostMediaSerializer(many=True)
+    comments = serializers.SerializerMethodField()
 
     class Meta:
         model = Post
         fields = (
+            "id",
             "caption",
             "user",
             "location",
             "media",
+            "comments",
         )
+
+    def get_comments(self, obj):
+        serializer = CommentListSerializer(
+            obj.comments.filter(reply_to__isnull=True), many=True
+        )
+        return serializer.data
