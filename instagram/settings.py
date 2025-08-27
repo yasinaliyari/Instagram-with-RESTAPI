20a3e53499a55c45b1bbdce64fb3d79ad8d44ef4
yasin aliyari <yasinaliyari30@gmail.com>
2025-08-27 15:59:05 +0330

    adjust throttle

--- instagram/settings.py
+++ instagram/settings.py
@@ -142,6 +142,11 @@ REST_FRAMEWORK = {
     # "PAGE_SIZE": 5,
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",
     "PAGE_SIZE": 15,
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {"anon": "3/m", "user": "5/m"},
 }
 
 SIMPLE_JWT = {
